9e8ac62b00a422c1980cea63d67592d302a9ce61
basilioruan <ruan_basilio@outlook.com>
2025-06-09 23:57:31 -0300

    feat: adding example img with text and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import Image from 'next/image';
+import roseImg from '../../public/assets/rose_img.jpg';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className={styles.container}>
+      <span className={styles.mainText}>Causando sorrisos e emoções </span>
+      <span className={styles.secondaryText}>Confira nossos produtos e serviços </span>
+      <button className={styles.productBtn}> Produtos </button>
+      <Image src={roseImg} className={styles.image} alt="Imagem rosa" />
+    </div>
   );
 }
