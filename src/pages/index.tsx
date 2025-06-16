b64d64c1852e86e246f021ecd162948736fdf648
basilioruan <ruan_basilio@outlook.com>
2025-06-15 23:45:06 -0300

    feat: adding a new section on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Image from 'next/image';
 import roseImg from '../../public/assets/rose_img.jpg';
+import servicesImg from '../../public/assets/services_img.jpg';
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
+    <>
     <div className={styles.container}>
-      <span className={styles.mainText}>Causando sorrisos e emoções </span>
-      <span className={styles.secondaryText}>Confira nossos produtos e serviços </span>
-      <button className={styles.productBtn}> Produtos </button>
-      <Image src={roseImg} className={styles.image} alt="Imagem rosa" />
-    </div>
+        <span className={styles.serviceMainText}> Causando sorrisos e emoções </span>
+        <span className={styles.serviceSecondaryText}> Confira nossos serviços </span>
+        <button className={styles.serviceBtn}> Serviços </button>
+        <Image src={servicesImg} className={styles.image} alt="Imagem decoração" />
+      </div>
+      <div className={styles.container}>
+        <span className={styles.productMainText}> Onde as flores se tornam sentimentos </span>
+        <span className={styles.productSecondaryText}> Confira nossos produtos </span>
+        <button className={styles.productBtn}> Produtos </button>
+        <Image src={roseImg} className={styles.image} alt="Imagem rosa" />
+      </div>
+      
+    </>
   );
 }
