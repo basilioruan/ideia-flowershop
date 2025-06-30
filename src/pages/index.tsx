f24b8313cfe60076defcab5d80d7205f9c378ef8
basilioruan <ruan_basilio@outlook.com>
2025-06-30 10:58:32 -0300

    style: adding responsiveness to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import roseImg from '../../public/assets/rose_img.jpg';
 import servicesImg from '../../public/assets/services_img.jpg';
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
-    <>
-    <div className={styles.container}>
-        <span className={styles.serviceMainText}> Causando sorrisos e emoções </span>
-        <span className={styles.serviceSecondaryText}> Confira nossos serviços </span>
-        <button className={styles.serviceBtn}> Serviços </button>
-        <Image src={servicesImg} className={styles.image} alt="Imagem decoração" />
-      </div>
+    <div className={styles.homeContainer}>
       <div className={styles.container}>
-        <span className={styles.productMainText}> Onde as flores se tornam sentimentos </span>
-        <span className={styles.productSecondaryText}> Confira nossos produtos </span>
-        <button className={styles.productBtn}> Produtos </button>
-        <Image src={roseImg} className={styles.image} alt="Imagem rosa" />
+        <Image src={servicesImg} className={styles.image} alt="Imagem decoração" />
+        <div className={`${styles.textContent} ${styles.serviceContent}`}>
+          <span className={styles.serviceMainText}>Causando sorrisos e emoções</span>
+          <span className={styles.serviceSecondaryText}>Confira nossos serviços</span>
+          <Link href="/servicos">
+            <button className={styles.serviceBtn}>Serviços</button>
+          </Link>
+        </div>
       </div>
       
-    </>
+      <div className={styles.container}>
+        <Image src={roseImg} className={styles.image} alt="Imagem rosa" />
+        <div className={`${styles.textContent} ${styles.productContent}`}>
+          <span className={styles.productMainText}>Onde as flores se tornam sentimentos</span>
+          <span className={styles.productSecondaryText}>Confira nossos produtos</span>
+          <Link href="/produtos">
+            <button className={styles.productBtn}>Produtos</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
